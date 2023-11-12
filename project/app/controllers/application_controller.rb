7426901c1aa6041d977640f0bc99a7aa4f025cd0
黄融亮 <21373488@buaa.edu.cn>
2023-11-12 23:40:53 +0800

    add admin register

--- project/app/controllers/application_controller.rb
+++ project/app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
 
 protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :avatar, :role])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :avatar])
   end
 
