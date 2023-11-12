baa605757ee49d808bf9304fc5b95c759a5442b0
黄融亮 <21373488@buaa.edu.cn>
2023-11-12 22:53:00 +0800

    fix pipeline2

--- project/config/environments/development.rb
+++ project/config/environments/development.rb
@@ -74,5 +74,5 @@ Rails.application.configure do
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.hosts << "21373488.blog4.rubyapp.act.buaa.edu.cn:9000"
+  config.hosts << "21373488.project.rubyapp.act.buaa.edu.cn:9000"
 end
